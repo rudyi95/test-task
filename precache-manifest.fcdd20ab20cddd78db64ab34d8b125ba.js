02c86bcacfe97c69fb7d24043448b085cbd18dce
rudyi95 <rud11@meta.ua>
2020-05-04 16:09:46 +0300

    Updates

---
+++ precache-manifest.fcdd20ab20cddd78db64ab34d8b125ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7579fc6ac8378fc4b752a6461faa374",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "fb759acc6ae165ea9bcd",
+    "url": "/test-task/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a022ec3d4a10df648ed7",
+    "url": "/test-task/static/css/main.0c5dcfa4.chunk.css"
+  },
+  {
+    "revision": "fb759acc6ae165ea9bcd",
+    "url": "/test-task/static/js/2.9f9d7fc0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/test-task/static/js/2.9f9d7fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a022ec3d4a10df648ed7",
+    "url": "/test-task/static/js/main.d1e16b96.chunk.js"
+  },
+  {
+    "revision": "35afc974da708d2af311",
+    "url": "/test-task/static/js/runtime-main.9c153a01.js"
+  }
+]);
